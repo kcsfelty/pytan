d6f86033db8d77a7d9486d2431bcaf337d18e64b
Ricky Felty <kcsfelty@gmail.com>
2023-03-10 02:12:40 -0600

    random testing2

--- pytan_fast/random_agents_test.py
+++ pytan_fast/random_agents_test.py
@@ -42,61 +42,19 @@ def train_eval(
 		batch_size=1000,
 		train_steps=5,
 	):
-	steps_per_iteration = int(batch_size * train_steps)
-	iterations = int(total_steps / steps_per_iteration)
 	global_step = np.zeros(1, dtype=np.int64)
-	eps_scaling = np.zeros(3)
-	eps_scaling.fill(1)
-	eps_base = 0.5
-	def epsilon_greedy(player_index):
-		def get_epsilon_greedy():
-			return eps_base * eps_scaling[player_index].item()
-		return get_epsilon_greedy
-
-
-	agent_list = [
-		FastAgent(
-			player_index=0,
-			batch_size=batch_size,
-			log_dir=log_dir,
-			replay_buffer_capacity=batch_size*train_steps,
-			fc_layer_params=(2 ** 5, 2 ** 5),
-			epsilon_greedy=epsilon_greedy(0),
-			env_specs=env_specs),
-		FastAgent(
-			player_index=1,
-			batch_size=batch_size,
-			log_dir=log_dir,
-			replay_buffer_capacity=batch_size*train_steps,
-			fc_layer_params=(2 ** 5, 2 ** 5),
-			epsilon_greedy=epsilon_greedy(1),
-			env_specs=env_specs),
-		FastAgent(
-			player_index=2,
-			batch_size=batch_size,
-			log_dir=log_dir,
-			replay_buffer_capacity=batch_size*train_steps,
-			fc_layer_params=(2 ** 5, 2 ** 5),
-			epsilon_greedy=epsilon_greedy(2),
-			env_specs=env_specs)]
 
 	policy_list = [RandomTFPolicy(
 		time_step_spec=env_specs["env_time_step_spec"],
 		action_spec=env_specs["env_action_spec"],
-	) for agent in agent_list]
-	observer_list = [[] for agent in agent_list]
-	summary_list = [agent.write_summary for agent in agent_list]
+		observation_and_action_constraint_splitter=splitter,)] * 3
+
+	observer_list = [[]] * 3
+	summary_list = None
 	env = PyTanFast(policy_list, observer_list, summary_list, global_step, log_dir)
 
-	for i in range(iterations):
-		print("starting iteration", i, "steps so far:", global_step.item(), str(int(global_step / total_steps * 1e2)) + "%",)
-		env.run(step_limit=steps_per_iteration)
-		for agent, a in zip(agent_list, range(len(agent_list))):
-			agent.train(train_steps)
-		if global_step >= 50000:
-			eps_base = 0.01
-			eps_scaling = eps_place_scaling[env.get_player_win_rate_order(50)]
-		print(eps_scaling)
+	print("Testing random agents")
+	env.run(step_limit=total_steps)
 
 def main():
 	policy_list = [Policy(), Policy(), Policy()]
